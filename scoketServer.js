e86c486446a4c4753ab52c92b01db7765776de55
swh <1055048972@qq.com>
2019-12-20 10:32:59 +0800

    添加单独websocket服务

---
+++ scoketServer.js
@@ -0,0 +1,24 @@
+const ws = require('nodejs-websocket')
+
+const createServer = () => {
+  let server = ws.createServer(connection => {
+    
+    connection.on('text', function(result) {
+      console.log('接收消息', result);
+      connection.sendText('sss');
+    })
+    connection.on('connect', function(code) {
+      console.log('开启连接', code)
+    })
+    connection.on('close', function(code) {
+      console.log('关闭连接', code)
+    })
+    connection.on('error', function(code) {
+      console.log('异常关闭', code)
+    })
+  })
+  return server
+}
+
+module.exports = createServer();
+createServer().listen(8000);;
